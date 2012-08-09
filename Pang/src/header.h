da4302d9992e0ed0ff5b4c8725331a2e121a13eb
Dylan Wragge <dylanwragge@gmail.com>
2012-08-09 21:48:32 +1200

    adding files

---
+++ Pang/src/header.h
@@ -0,0 +1,40 @@
+// header.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <tchar.h>
+#include <ctime>
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/OpenGL.hpp>
+
+#include <map>
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <sstream>
+
+/*
+#ifdef _DEBUG
+#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+#pragma comment(lib, "sfml-audio-d.lib")
+
+#else
+#pragma comment(lib, "sfml-graphics-s.lib")
+#pragma comment(lib, "sfml-system-s.lib")
+#pragma comment(lib, "sfml-window-s.lib")
+#pragma comment(lib, "sfml-audio-s.lib")
+#endif
+*/
+
+
+// TODO: reference additional headers your program requires here
